011c8908acc84a890793ce94f60faccc93353724
PULKIT OJHA <42231120+TheCYBORGIANpulkit@users.noreply.github.com>
2020-10-04 22:49:08 +0530

    Update Ass5_q1a.cpp

--- Ass5_q1a.cpp
+++ Ass5_q1a.cpp
@@ -21,4 +21,19 @@ int main(){
     False_position(f,1.5,3.25);
     Newton_Raphson(f,3);
     return 0;
+    /*
+    Hello
+    after bisection
+    c:2.69626
+    f(c):-4.17233e-007
+    Please find the attached csv file generated and the plot
+    after false-positioning
+    c:2.69626
+    f(c):-1.78814e-007
+    Please find the attached csv file generated and the plot
+    after newton-raphson
+    root:2.69627
+    f(x): 9.98378e-006
+    Please find the attached csv file generated and the plot
+    */
 }
